b2327ecd9e98512c538e04a9298526cb4f481183
Mark Stenglein <mark@stengle.in>
2017-10-16 22:14:26 -0400

    Build: JS: app.js: Add client side DNS checking
    This patch allows the Chromecasts to pull from Local DNS if
    the Channel's iFrames point to sites that are on the company intranet.
    
    The chromecasts themselves are locked to google's DNS for security
    reasons (keep you from serving hacked resources to them I suppose),
    which is why intranet sites need to be pulled from the Multicast
    server instead.
    
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- build/js/app.js
+++ build/js/app.js
@@ -17,3 +17,69 @@ function notify(opts) {
     })
   }, dur)
 }
+
+function serverReachable(url) {
+  // IE vs. standard XHR creation
+  var x = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" ),
+      s;
+  x.open(
+    // requesting the headers is faster, and just enough
+    "HEAD",
+    url,
+    // make a synchronous request
+    false
+  );
+  try {
+    x.send();
+    s = x.status;
+    // Make sure the server is reachable
+    return ( s >= 200 && s < 300 || s === 304 );
+  // catch network & other problems
+  } catch (e) {
+    return false;
+  }
+}
+
+function fixLocalDNS(iframe) {
+  let l = document.createElement("a")
+  l.href = iframe.src
+
+  /* ask the server for the dns resolution */
+  $.getJSON(
+    window.location.origin + "/dns?hostname=" + l.hostname,
+    function (data) {
+      if (data.err) {
+        console.err(data.err)
+      }
+      else {
+      /* set the url to instead use the IP address */
+        let src = l.protocol+ "//" + data.address
+        if (l.port) {
+          src += ":" + l.port
+        }
+        if (l.path) {
+          src += "/" + l.path
+        }
+        if (l.search) {
+          src += l.search
+        }
+        /* set the new url in the iFrame */
+        iframe.src = src
+      }
+    })
+}
+
+let i, frames;
+frames = document.getElementsByTagName("iframe");
+/* go through all iFrames in the page and check DNS */
+for (i = 0; i < frames.length; ++i)
+{
+  let href = frames[i].src
+
+  let l = document.createElement("a");
+  l.href = href;
+  
+  if (!serverReachable(l.href)) {
+    fixLocalDNS(frames[i], l.hostname)
+  }
+}
